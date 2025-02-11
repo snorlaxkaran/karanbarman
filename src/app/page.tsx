77fff4193d2cc9f9127cbff0f2eb86120f5092aa
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-02-11 10:46:22 +0530

    added verify badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import ProjectsSvg from "@/data/projects";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import Check from "@/data/icons/check";
 
 export default function Home() {
   return (
@@ -23,25 +24,31 @@ export default function Home() {
                 className="rounded-full"
               />
             </div>
-            <div className="flex justify-between">
-              <h1 className="text-3xl font-bold">Hi, I&apos;m Karan</h1>
-              <div className="flex gap-2">
-                <Link href={"https://www.youtube.com/@codewithkaran17"}>
+            <div className="flex justify-between items-center">
+              <div>
+                <h1 className="text-3xl font-bold">Hi, I&apos;m Karan</h1>
+                <Link
+                  href={"https://www.youtube.com/@codewithkaran17"}
+                  className="flex items-center gap-x-1 text-blue-600"
+                >
                   <Image
                     src={"/svgs/youtube1.svg"}
                     alt="youtube"
                     width={100}
                     height={100}
-                    className="size-9"
-                  />
+                    className="size-5"
+                  />{" "}
+                  @codewithkaran17
                 </Link>
+              </div>
+              <div className="flex gap-2">
                 <div className="border rounded-full border-neutral-500 p-2">
                   <Mail className="size-5 text-neutral-500" />
                 </div>
               </div>
             </div>
             <div>
-              <p className="text-muted-foreground">
+              <p className="text-[#666]">
                 {/* TODO: SUMMARY */}Full Stack Developer with over a year of
                 experience building scalable, production-ready applications.
                 Currently developing a document editor to simplify collaborative
@@ -88,8 +95,9 @@ export default function Home() {
               </div>
               {/* Education Details */}
               <div className="flex flex-col">
-                <div className="text-[#555] ">
-                  <h2>Samyak Infotech</h2>
+                <div className=" flex gap-1 items-center">
+                  <h2 className="text-[#555]">Samyak Infotech</h2>
+                  <Check />
                 </div>
                 <div>
                   <p className="text-[#333] text-lg">Frontend Developer</p>
@@ -113,8 +121,9 @@ export default function Home() {
               </div>
               {/* Education Details */}
               <div className="flex flex-col">
-                <div className="text-[#555] ">
-                  <h2>Maa Vaishno Interiors</h2>
+                <div className=" flex gap-1 items-center">
+                  <h2 className="text-[#555]">Maa Vaishno Interiors</h2>
+                  <Check />
                 </div>
                 <div>
                   <p className="text-[#333] text-lg">Fullstack Developer</p>
@@ -189,222 +198,6 @@ export default function Home() {
               <h1 className="text-2xl font-medium">Projects</h1>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <Card>
-                <div className=" relative w-full aspect-video">
-                  <Image
-                    src={"/nextjs.png"}
-                    alt="project"
-                    width={100}
-                    height={100}
-                    style={{
-                      position: "absolute",
-                      height: "100%",
-                      width: "100%",
-                      inset: "0px",
-                      color: "transparent",
-                    }}
-                    unoptimized
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5 p-6 px-2 py-2 overflow-hidden">
-                  <h3 className="tracking-tight text-base sm:text-lg font-medium flex items-center justify-between gap-x-2">
-                    <span>Slugy</span>
-                  </h3>
-                  <div className="text-xs flex items-center justify-between">
-                    <p>Oct 2024 - Present</p>
-                  </div>
-                </div>
-                <p className="px-2 overflow-hidden text-xs text-gray-500 dark:text-gray-300">
-                  Slugy is an open-source SaaS tool for fast, secure, and easy
-                  link management, tracking and organization for individuals and
-                  businesses.
-                </p>
-                <div className="p-2 pt-3 overflow-hidden flex items-center flex-wrap gap-1">
-                  <Button size={"sm"} variant={"outline"}>
-                    Next.js
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    TypeScript
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    Prisma
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    PostgreSQL
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    AWS
-                  </Button>
-                </div>
-                <div className="p-6 pt-0 flex items-center gap-2 px-2 pb-2 overflow-hidden">
-                  <Button size={"sm"}>
-                    <Globe />
-                    Webiste
-                  </Button>
-                </div>
-              </Card>
-              <Card>
-                <div className=" relative w-full aspect-video">
-                  <Image
-                    src={"/nextjs.png"}
-                    alt="project"
-                    width={100}
-                    height={100}
-                    style={{
-                      position: "absolute",
-                      height: "100%",
-                      width: "100%",
-                      inset: "0px",
-                      color: "transparent",
-                    }}
-                    unoptimized
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5 p-6 px-2 py-2 overflow-hidden">
-                  <h3 className="tracking-tight text-base sm:text-lg font-medium flex items-center justify-between gap-x-2">
-                    <span>Slugy</span>
-                  </h3>
-                  <div className="text-xs flex items-center justify-between">
-                    <p>Oct 2024 - Present</p>
-                  </div>
-                </div>
-                <p className="px-2 overflow-hidden text-xs text-gray-500 dark:text-gray-300">
-                  Slugy is an open-source SaaS tool for fast, secure, and easy
-                  link management, tracking and organization for individuals and
-                  businesses.
-                </p>
-                <div className="p-2 pt-3 overflow-hidden flex items-center flex-wrap gap-1">
-                  <Button size={"sm"} variant={"outline"}>
-                    Next.js
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    TypeScript
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    Prisma
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    PostgreSQL
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    AWS
-                  </Button>
-                </div>
-                <div className="p-6 pt-0 flex items-center gap-2 px-2 pb-2 overflow-hidden">
-                  <Button size={"sm"}>
-                    <Globe />
-                    Webiste
-                  </Button>
-                </div>
-              </Card>
-              <Card>
-                <div className=" relative w-full aspect-video">
-                  <Image
-                    src={"/nextjs.png"}
-                    alt="project"
-                    width={100}
-                    height={100}
-                    style={{
-                      position: "absolute",
-                      height: "100%",
-                      width: "100%",
-                      inset: "0px",
-                      color: "transparent",
-                    }}
-                    unoptimized
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5 p-6 px-2 py-2 overflow-hidden">
-                  <h3 className="tracking-tight text-base sm:text-lg font-medium flex items-center justify-between gap-x-2">
-                    <span>Slugy</span>
-                  </h3>
-                  <div className="text-xs flex items-center justify-between">
-                    <p>Oct 2024 - Present</p>
-                  </div>
-                </div>
-                <p className="px-2 overflow-hidden text-xs text-gray-500 dark:text-gray-300">
-                  Slugy is an open-source SaaS tool for fast, secure, and easy
-                  link management, tracking and organization for individuals and
-                  businesses.
-                </p>
-                <div className="p-2 pt-3 overflow-hidden flex items-center flex-wrap gap-1">
-                  <Button size={"sm"} variant={"outline"}>
-                    Next.js
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    TypeScript
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    Prisma
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    PostgreSQL
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    AWS
-                  </Button>
-                </div>
-                <div className="p-6 pt-0 flex items-center gap-2 px-2 pb-2 overflow-hidden">
-                  <Button size={"sm"}>
-                    <Globe />
-                    Webiste
-                  </Button>
-                </div>
-              </Card>
-              <Card>
-                <div className=" relative w-full aspect-video">
-                  <Image
-                    src={"/nextjs.png"}
-                    alt="project"
-                    width={100}
-                    height={100}
-                    style={{
-                      position: "absolute",
-                      height: "100%",
-                      width: "100%",
-                      inset: "0px",
-                      color: "transparent",
-                    }}
-                    unoptimized
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5 p-6 px-2 py-2 overflow-hidden">
-                  <h3 className="tracking-tight text-base sm:text-lg font-medium flex items-center justify-between gap-x-2">
-                    <span>Slugy</span>
-                  </h3>
-                  <div className="text-xs flex items-center justify-between">
-                    <p>Oct 2024 - Present</p>
-                  </div>
-                </div>
-                <p className="px-2 overflow-hidden text-xs text-gray-500 dark:text-gray-300">
-                  Slugy is an open-source SaaS tool for fast, secure, and easy
-                  link management, tracking and organization for individuals and
-                  businesses.
-                </p>
-                <div className="p-2 pt-3 overflow-hidden flex items-center flex-wrap gap-1">
-                  <Button size={"sm"} variant={"outline"}>
-                    Next.js
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    TypeScript
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    Prisma
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    PostgreSQL
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    AWS
-                  </Button>
-                </div>
-                <div className="p-6 pt-0 flex items-center gap-2 px-2 pb-2 overflow-hidden">
-                  <Button size={"sm"}>
-                    <Globe />
-                    Webiste
-                  </Button>
-                </div>
-              </Card>
               <Card>
                 <div className=" relative w-full aspect-video">
                   <Image

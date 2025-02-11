e4de0d7fc5d6ad9c052906a7c6fc9e56e259699c
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-02-11 10:25:51 +0530

    added exp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,6 +74,52 @@ export default function Home() {
             <div>
               <h1 className="text-2xl font-medium">Experience</h1>
             </div>
+            <div className="flex gap-2">
+              {/* Graduate Icon Container */}
+              <div className="rounded-full w-9 h-9 border p-1 bg-gray-100 dark:bg-gray-500 flex items-center justify-center">
+                <Image
+                  src={"/samyak.png"}
+                  alt="hat"
+                  width={20}
+                  height={20}
+                  className="block"
+                  unoptimized
+                />
+              </div>
+              {/* Education Details */}
+              <div className="flex flex-col">
+                <div className="text-lg text-[#555] ">
+                  <h2>Samyak Infotech</h2>
+                </div>
+                <div>
+                  <p className="text-[#333] text-xl">Frontend Developer</p>
+                  <p className="text-[#777] text-sm">Mar 2024 - Nov 2024</p>
+                </div>
+              </div>
+            </div>
+            <div className="flex gap-2">
+              {/* Graduate Icon Container */}
+              <div className="rounded-full w-9 h-9 border p-1 bg-gray-100 dark:bg-gray-500 flex items-center justify-center">
+                <Image
+                  src={"/samyak.png"}
+                  alt="hat"
+                  width={20}
+                  height={20}
+                  className="block"
+                  unoptimized
+                />
+              </div>
+              {/* Education Details */}
+              <div className="flex flex-col">
+                <div className="text-lg text-[#555] ">
+                  <h2>Samyak Infotech</h2>
+                </div>
+                <div>
+                  <p className="text-[#333] text-xl">Frontend Developer</p>
+                  <p className="text-[#777] text-sm">Mar 2024 - Nov 2024</p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="border-b border-x border-neutral-300/80 py-10">
@@ -139,114 +185,6 @@ export default function Home() {
               <h1 className="text-2xl font-medium">Projects</h1>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <Card>
-                <div className=" relative w-full aspect-video">
-                  <Image
-                    src={"/nextjs.png"}
-                    alt="project"
-                    width={100}
-                    height={100}
-                    style={{
-                      position: "absolute",
-                      height: "100%",
-                      width: "100%",
-                      inset: "0px",
-                      color: "transparent",
-                    }}
-                    unoptimized
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5 p-6 px-2 py-2 overflow-hidden">
-                  <h3 className="tracking-tight text-base sm:text-lg font-medium flex items-center justify-between gap-x-2">
-                    <span>Slugy</span>
-                  </h3>
-                  <div className="text-xs flex items-center justify-between">
-                    <p>Oct 2024 - Present</p>
-                  </div>
-                </div>
-                <p className="px-2 overflow-hidden text-xs text-gray-500 dark:text-gray-300">
-                  Slugy is an open-source SaaS tool for fast, secure, and easy
-                  link management, tracking and organization for individuals and
-                  businesses.
-                </p>
-                <div className="p-2 pt-3 overflow-hidden flex items-center flex-wrap gap-1">
-                  <Button size={"sm"} variant={"outline"}>
-                    Next.js
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    TypeScript
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    Prisma
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    PostgreSQL
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    AWS
-                  </Button>
-                </div>
-                <div className="p-6 pt-0 flex items-center gap-2 px-2 pb-2 overflow-hidden">
-                  <Button size={"sm"}>
-                    <Globe />
-                    Webiste
-                  </Button>
-                </div>
-              </Card>
-              <Card>
-                <div className=" relative w-full aspect-video">
-                  <Image
-                    src={"/nextjs.png"}
-                    alt="project"
-                    width={100}
-                    height={100}
-                    style={{
-                      position: "absolute",
-                      height: "100%",
-                      width: "100%",
-                      inset: "0px",
-                      color: "transparent",
-                    }}
-                    unoptimized
-                  />
-                </div>
-                <div className="flex flex-col space-y-1.5 p-6 px-2 py-2 overflow-hidden">
-                  <h3 className="tracking-tight text-base sm:text-lg font-medium flex items-center justify-between gap-x-2">
-                    <span>Slugy</span>
-                  </h3>
-                  <div className="text-xs flex items-center justify-between">
-                    <p>Oct 2024 - Present</p>
-                  </div>
-                </div>
-                <p className="px-2 overflow-hidden text-xs text-gray-500 dark:text-gray-300">
-                  Slugy is an open-source SaaS tool for fast, secure, and easy
-                  link management, tracking and organization for individuals and
-                  businesses.
-                </p>
-                <div className="p-2 pt-3 overflow-hidden flex items-center flex-wrap gap-1">
-                  <Button size={"sm"} variant={"outline"}>
-                    Next.js
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    TypeScript
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    Prisma
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    PostgreSQL
-                  </Button>
-                  <Button size={"sm"} variant={"outline"}>
-                    AWS
-                  </Button>
-                </div>
-                <div className="p-6 pt-0 flex items-center gap-2 px-2 pb-2 overflow-hidden">
-                  <Button size={"sm"}>
-                    <Globe />
-                    Webiste
-                  </Button>
-                </div>
-              </Card>
               <Card>
                 <div className=" relative w-full aspect-video">
                   <Image

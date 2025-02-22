a44ddc10a09c83b04a80cb34b5fce2103abf87ee
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-02-22 08:59:24 +0530

    kok

--- src/components/shared/card.tsx
+++ src/components/shared/card.tsx
@@ -1,71 +1,80 @@
-import { Globe } from "lucide-react";
-import Image from "next/image";
-import { Card } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+// import { Globe, LucideIcon } from "lucide-react";
+// import Image from "next/image";
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardTitle,
+// } from "@/components/ui/card";
+// import { Button } from "@/components/ui/button";
+// import { cn } from "@/lib/utils";
 
-interface Skill {
-  name: string;
-}
+// interface Skill {
+//   name: string;
+//   value: string;
+//   icon: LucideIcon;
+// }
 
-interface CardTempProps {
-  imageUrl: string;
-  namePr: string;
-  duration: string;
-  desc: string;
-  skills: Skill[];
-}
+// interface CardTempProps {
+//   imageUrl: string;
+//   namePr: string;
+//   duration: string;
+//   desc: string;
+//   skills: Skill[];
+// }
 
-const CardTemp = ({
-  imageUrl,
-  namePr,
-  duration,
-  desc,
-  skills,
-}: CardTempProps) => {
-  return (
-    <Card>
-      <div className=" relative w-full aspect-video">
-        <Image
-          src={imageUrl}
-          alt="project"
-          width={100}
-          height={100}
-          style={{
-            position: "absolute",
-            height: "100%",
-            width: "100%",
-            inset: "0px",
-            color: "transparent",
-          }}
-          unoptimized
-        />
-      </div>
-      <div className="flex flex-col space-y-1.5 p-6 px-2 py-2 overflow-hidden">
-        <h3 className="tracking-tight text-base sm:text-lg font-medium flex items-center justify-between gap-x-2">
-          <span>{namePr}</span>
-        </h3>
-        <div className="text-xs flex items-center justify-between">
-          <p>{duration}</p>
-        </div>
-      </div>
-      <p className="px-2 overflow-hidden text-xs text-gray-500 dark:text-gray-300">
-        {desc}
-      </p>
-      <div className="p-2 pt-3 overflow-hidden flex items-center flex-wrap gap-1">
-        {skills.map((skill, i) => (
-          <Button key={i} size={"sm"} variant={"outline"}>
-            {skill.name}
-          </Button>
-        ))}
-      </div>
-      <div className="p-6 pt-0 flex items-center gap-2 px-2 pb-2 overflow-hidden">
-        <Button size={"sm"}>
-          <Globe />
-          Website
-        </Button>
-      </div>
-    </Card>
-  );
-};
+// const CardTemp = (
+//   { imageUrl, namePr, duration, desc, skills }: CardTempProps,
+//   { name, value, icon: Icon }: Skill
+// ) => {
+//   return (
+//     <Card className="relative">
+//       <Image
+//         src={"/Udemy.jpg"}
+//         alt="kok"
+//         width={100}
+//         height={100}
+//         className="w-full"
+//         unoptimized
+//       />
+//       <CardContent className="flex flex-col space-y-3 mt-6">
+//         <CardTitle className="font-medium">{namePr}</CardTitle>
+//         <CardDescription className="flex flex-col space-y-3">
+//           <p className="">{duration}</p>
+//           <p className="text-sm">{desc}</p>
+//           <div className="flex gap-1 flex-wrap">
+//             {skills.map((skill) => (
+//               <Button
+//                 key={skill.value}
+//                 variant={"outline"}
+//                 size={"sm"}
+//                 className="flex items-center text-black"
+//               >
+//                 <Image
+//                   src={`/svgs/${imageUrl}.svg`}
+//                   alt={skill.name}
+//                   width={100}
+//                   height={100}
+//                   className={cn(
+//                     "w-4 h-4",
+//                     namePr === "react" && "animate-spin-slow"
+//                   )}
+//                 />
+//                 <skill.I
+//                 {skill.name}
+//               </Button>
+//             ))}
+//           </div>
+//         </CardDescription>
+//         <div>
+//           <Button size={"sm"}>
+//             <Globe />
+//             Website
+//           </Button>
+//         </div>
+//       </CardContent>
+//     </Card>
+//   );
+// };
 
-export default CardTemp;
+// export default CardTemp;

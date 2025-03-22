ff413b181891bf4ddf6d78f816cbc7869fd8707e
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-03-23 00:17:10 +0530

    ae

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -15,49 +15,9 @@ export const skills = [
     image: "typescript",
   },
   {
-    name: "Node.js",
-    value: "node",
-    image: "node",
-  },
-  {
     name: "JavaScript",
     value: "javascript",
     image: "javascript",
-  },
-  {
-    name: "MongoDB",
-    value: "mongodb",
-    image: "mongodb",
-  },
-  {
-    name: "AWS",
-    value: "aws",
-    image: "aws",
-  },
-  {
-    name: "PostgreSQL",
-    value: "postgresql",
-    image: "postgresql",
-  },
-  {
-    name: "Prisma",
-    value: "prisma",
-    image: "prisma",
-  },
-  {
-    name: "Drizzle",
-    value: "drizzle",
-    image: "drizzle",
-  },
-  {
-    name: "Hono",
-    value: "hono",
-    image: "hono",
-  },
-  {
-    name: "Express",
-    value: "express",
-    image: "express",
   },
   {
     name: "Zod",
@@ -75,9 +35,61 @@ export const skills = [
     image: "redux",
   },
   {
+    name: "Recoil",
+    value: "recoil",
+    image: "recoil",
+  },
+  {
+    name: "Node.js",
+    value: "node",
+    image: "node",
+  },
+  {
+    name: "Hono",
+    value: "hono",
+    image: "hono",
+  },
+  {
+    name: "Express",
+    value: "express",
+    image: "express",
+  },
+  {
+    name: "MongoDB",
+    value: "mongodb",
+    image: "mongodb",
+  },
+
+  {
+    name: "PostgreSQL",
+    value: "postgresql",
+    image: "postgresql",
+  },
+  {
+    name: "Prisma",
+    value: "prisma",
+    image: "prisma",
+  },
+  {
+    name: "Drizzle",
+    value: "drizzle",
+    image: "drizzle",
+  },
+
+  {
     name: "Docker",
     value: "docker",
     image: "docker",
+  },
+  {
+    name: "AWS",
+    value: "aws",
+    image: "aws",
+  },
+  {
+    name: "Azure",
+    value: "azure",
+    image: "azure",
   },
   {
     name: "Cloudfare",

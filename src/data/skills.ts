3818e22633f14d51ff4056843f27f38a9f99453f
snorlax.karan@gmail.com <snorlax.karan@gmail.com>
2025-04-13 21:34:29 +0530

    added rabbitmq

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -65,6 +65,11 @@ export const skills = [
     image: "postgresql",
   },
   {
+    name: "RabbitMQ",
+    value: "rabbitmq",
+    image: "rabbitmq",
+  },
+  {
     name: "Redis",
     value: "redis",
     image: "redis",
